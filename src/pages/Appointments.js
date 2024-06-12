d0a6a2ae36dc638bab6f90a81d2e3087db82d919
Filip Marić <fmaric1@etf.unsa.ba>
2024-06-12 09:48:46 +0200

    Splitted signup for patient and dentist

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -5,29 +5,7 @@ const Appointments = () => {
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        try {
-            const response = await fetch('http://localhost:8091/myappointments', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            console.log('Login succesfull', data.token);
-            localStorage.setItem(token, data.token)
-            navigate('/');
-        } catch (error) {
-            console.error('Login failed: ', error);
-        }
+       
 
         fetchAppointments();
     }, []);
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const Signup = () => {
 
@@ -9,14 +12,24 @@ const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [keyword, setKeyword] = useState('');
+    const [gender, setGender] = useState('Male');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [birthDate, setBirthDate] = useState(new Date());
+    const [workingHours, setWorkingHours] = useState('');
     const [role, setRole] = useState('patient'); // Default role is 'patient'
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
+        const isDoctor = role === "patient" ? 0 : 1;
         e.preventDefault();
         // Check for empty fields
-        if (!firstName || !lastName || !email || !password) {
+        if (isDoctor && (!firstName || !lastName || !email || !password || !workingHours)) {
+            setErrorMessage('Please fill in all required fields.');
+            return; // Stop execution
+        }
+        if (!isDoctor && (!firstName || !lastName || !email || !password || !phone || !address)) {
             setErrorMessage('Please fill in all required fields.');
             return; // Stop execution
         }
@@ -26,26 +39,57 @@ const Signup = () => {
             return; // Stop execution
         }
         try {
-            const response = await fetch('http://localhost:8091/auth/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password,
-                    firstName,
-                    lastName
-                })
-            });
+            if (isDoctor) {
+                const response = await fetch('http://localhost:8091/auth/signup/dentist', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email,
+                        password,
+                        firstName,
+                        lastName,
+                        workingHours
+                    })
+                });
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                const data = await response.json();
+                console.log('User signed up successfully:', data);
+                navigate('/login')
+            }
+            else {
+
+                const response = await fetch('http://localhost:8091/auth/signup/patient', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email,
+                        password,
+                        firstName,
+                        lastName,
+                        birthDate,
+                        phone,
+                        gender,
+                        address
+
+                    })
+                });
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                console.log('User signed up successfully:', data);
+                navigate('/login')
             }
 
-            const data = await response.json();
-            console.log('User signed up successfully:', data);
-            navigate('/login')
+           
         } catch (error) {
             console.error('Error signing up user:', error);
         }
@@ -91,11 +135,53 @@ const Signup = () => {
                     value={keyword}
                     onChange={(e) => setKeyword(e.target.value)}
                     placeholder="Keyword"
+                />)}
+           
+            <br></br>
+            {role === 'dentist' && (
+                <input
+                    type="text"
+                    value={workingHours}
+                    onChange={(e) => setWorkingHours(e.target.value)}
+                    placeholder="Working Hours"
                 />
+
+            )}
+            {role === 'patient' && (
+                <DatePicker selected={birthDate} onChange={(date) => setBirthDate(date)} />
+
+            )}
+
+            {role === 'patient' && (
+                <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    placeholder="Phone Number"
+                />
+
+            )}
+
+            
+            {role === 'patient' && (
+                <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    placeholder="Address"
+                />
+
+            )}
+            {role === 'patient' && (
+                <select value={gender} onChange={(e) => setGender(e.target.value)}>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+
             )}
 
             {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-
+            <br></br>
             <button type="submit">Signup</button>
         </form>
     );
